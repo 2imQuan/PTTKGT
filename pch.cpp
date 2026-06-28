16c2bf93b1b1a3d17997e09ac8ee2307485aa3b1
dnam6626 <duynam16032006z@gmail.com>
2026-06-28 23:41:26 +0700

    Feat: Complete core pipeline for WinUI 3 + Win2D

---
+++ pch.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Dispatching.h>
+#include <winrt/Microsoft.Graphics.Canvas.UI.Xaml.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.Foundation.h>
